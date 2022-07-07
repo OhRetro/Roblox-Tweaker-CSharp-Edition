d1c859ccac759ee8246edb0981173613468881d4
OhRetro <gabrielnacazone@hotmail.com>
2022-07-07 11:33:50 -0300

    a

--- publish.bat
+++ publish.bat
@@ -1,12 +1,2 @@
 @echo off
-set arg1=%1
-
-if "%arg1%" == "" (
-    dotnet publish --runtime win-x64 --configuration Debug -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
-)
-if "%arg1%" == "d" (
-    dotnet publish --runtime win-x64 --configuration Debug -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
-)
-if "%arg1%" == "r" (
-    dotnet publish --runtime win-x64 --configuration Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
-)
+dotnet publish --runtime win-x64 --configuration Release -p:PublishSingleFile=true
