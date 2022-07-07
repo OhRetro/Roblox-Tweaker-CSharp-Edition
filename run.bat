0774897071ab9452c1a67ed7bc0f0bb3a9af7920
OhRetro <gabrielnacazone@hotmail.com>
2022-07-06 22:23:05 -0300

    make my life easier

--- run.bat
+++ run.bat
@@ -1,2 +1,12 @@
 @echo off
-.\bin\Debug\net6.0\RTCSharpEdition.exe
+set arg1=%1
+
+if "%arg1%" == "" (
+    .\bin\Debug\net6.0\RTCSharpEdition.exe
+)
+if "%arg1%" == "d" (
+    .\bin\Debug\net6.0\RTCSharpEdition.exe
+)
+if "%arg1%" == "r" (
+    .\bin\Release\net6.0\RTCSharpEdition.exe
+)
